f80d5cf408ff5cb0318fd148d4b0cd85cfe6df90
Ivan Petkov <ivan.slv.petkov@gmail.com>
2023-12-22 12:57:17 +0200

    Modified App component so it includes router, global error context and styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+import NotFound from "./components/pages/NotFound";
+import Home from "./components/pages/Home";
+import Employees from "./components/pages/Employees";
+import Projects from "./components/pages/Projects";
+import Navigation from "./components/atoms/Navigation";
+import Assign from "./components/pages/Assign";
+import EmployeeDetails from "./components/molecules/EmployeeDetails";
+import EmployeeEdit from "./components/molecules/EmployeeEdit";
+import ErrorProvider from "./providers/ErrorProvider";
+import Error from "./components/atoms/Error";
+import ProjectEdit from "./components/molecules/ProjectEdit";
+import ProjectDetails from "./components/molecules/ProjectDetails";
+import Tasks from "./components/pages/Tasks";
+import Footer from "./components/atoms/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Content>
+        <ErrorProvider>
+          <Error />
+          <Navigation />
+          <Routes>
+            <Route path="*" element={<NotFound />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/employees" element={<Employees />} />
+            <Route path="/employees/:id" element={<EmployeeDetails />} />
+            <Route path="/employees/:id/edit" element={<EmployeeEdit />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/projects/:id" element={<ProjectDetails />} />
+            <Route path="/projects/:id/edit" element={<ProjectEdit />} />
+            <Route path="/tasks" element={<Tasks />} />
+            <Route path="/assign" element={<Assign />} />
+          </Routes>
+        </ErrorProvider>
+      </Content>
+      <Footer />
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const Content = styled.div`
+  max-width: 1200px;
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+`;
 
 export default App;
